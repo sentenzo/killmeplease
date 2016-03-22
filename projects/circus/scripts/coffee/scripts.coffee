61a32f497a629585a2f232a395f48a3df7f9275a
timushev.roman <kassiyskley@rambler.ru>
2016-03-22 13:47:13 +0300

    the tabs logic is more flexible now

--- projects/circus/scripts/coffee/scripts.coffee
+++ projects/circus/scripts/coffee/scripts.coffee
@@ -1,38 +1,35 @@
 #   cfcoptions : { "out": "../js/"   }  
 
-tabs = document.querySelectorAll('.tab')
-tabContents = document.querySelectorAll('.tabContent')
-
-tabsInit = () ->
-    tabs = document.querySelectorAll('.tab')
-    tabContents = document.querySelectorAll('.tabContent')
+@tabsInit = (tabClassName = 'tab', contentClassName = 'tabContent') ->
+    tabs = document.querySelectorAll('.' + tabClassName)
+    tabContents = document.querySelectorAll('.' + contentClassName)
     clickTab = (tab) ->
         t.classList.remove("active") for t in tabs
         tab.classList.add("active")
         tabName = ""
+        re = new RegExp(tabClassName + "\\d");
         for cName in tab.classList
-            if cName.match(/tab\d/) != null
+            if cName.match(re) != null
                 tabName = cName
-                    break
-    for tc in tabContents
-        tc.classList.remove("active")
-        tc = document.querySelectorAll(".tabContent."+tabName)
-        tci.classList.add("active") for tci in tc
+                break
+        for tc in tabContents
+            tc.classList.remove("active")
+            tc = document.querySelectorAll('.' + contentClassName + '.' + tabName)
+            tci.classList.add("active") for tci in tc
     for tab in tabs
         tab.onclick = () -> clickTab(this)
+        
 
-trigs = document.querySelectorAll(".trigger")
-trigged = document.querySelectorAll(".trigged")
-
-triggersInit = () ->
-    trigs = document.querySelectorAll(".trigger")
-    trigged = document.querySelectorAll(".trigged")
+@triggersInit = (trigClassname = "trigger", trigContentName = "trigged") ->
+    trigs = document.querySelectorAll("." + trigClassname)
+    trigged = document.querySelectorAll("." + trigContentName)
         
     clickTrig = (trig) ->
         trigName = ""
         trigState = "off"
+        re = new RegExp(trigClassname + "\\d");
         for cName in trig.classList
-            if cName.match(/trigger\d/) != null
+            if cName.match(re) != null
                 trigName = cName
             if cName == "off" || cName == "on"
                 trigState = cName
@@ -46,7 +43,7 @@ triggersInit = () ->
             trig.classList.remove("off")
             trig.classList.add("on")
             trigState = "on"
-        for td in document.querySelectorAll(".trigged")
+        for td in document.querySelectorAll("." + trigContentName)
             if td.classList.contains(trigName)
                 if trigState == "on"
                     td.classList.add("active")
@@ -54,8 +51,8 @@ triggersInit = () ->
                     td.classList.remove("active")
     for t in trigs
         t.onclick = () -> clickTrig(this)
-
+###
 window.onload = () ->
-    tabsInit()
-    triggersInit()
-
+    tabsInit("tab")
+    triggersInit("tabContent")
+###
--- projects/circus/scripts/js/scripts.js
+++ projects/circus/scripts/js/scripts.js
@@ -1,39 +1,45 @@
 (function() {
-  var tabContents, tabs, tabsInit, trigged, triggersInit, trigs;
-
-  tabs = document.querySelectorAll('.tab');
-
-  tabContents = document.querySelectorAll('.tabContent');
-
-  tabsInit = function() {
-    var clickTab, tab, _i, _len, _results;
-    tabs = document.querySelectorAll('.tab');
-    tabContents = document.querySelectorAll('.tabContent');
+  this.tabsInit = function(tabClassName, contentClassName) {
+    var clickTab, tab, tabContents, tabs, _i, _len, _results;
+    if (tabClassName == null) {
+      tabClassName = 'tab';
+    }
+    if (contentClassName == null) {
+      contentClassName = 'tabContent';
+    }
+    tabs = document.querySelectorAll('.' + tabClassName);
+    tabContents = document.querySelectorAll('.' + contentClassName);
     clickTab = function(tab) {
-      var cName, t, tabName, tc, tci, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref, _results;
+      var cName, re, t, tabName, tc, tci, _i, _j, _k, _len, _len1, _len2, _ref, _results;
       for (_i = 0, _len = tabs.length; _i < _len; _i++) {
         t = tabs[_i];
         t.classList.remove("active");
       }
       tab.classList.add("active");
       tabName = "";
+      re = new RegExp(tabClassName + "\\d");
       _ref = tab.classList;
       for (_j = 0, _len1 = _ref.length; _j < _len1; _j++) {
         cName = _ref[_j];
-        if (cName.match(/tab\d/) !== null) {
+        if (cName.match(re) !== null) {
           tabName = cName;
           break;
         }
       }
+      _results = [];
       for (_k = 0, _len2 = tabContents.length; _k < _len2; _k++) {
         tc = tabContents[_k];
         tc.classList.remove("active");
-      }
-      tc = document.querySelectorAll(".tabContent." + tabName);
-      _results = [];
-      for (_l = 0, _len3 = tc.length; _l < _len3; _l++) {
-        tci = tc[_l];
-        _results.push(tci.classList.add("active"));
+        tc = document.querySelectorAll('.' + contentClassName + '.' + tabName);
+        _results.push((function() {
+          var _l, _len3, _results1;
+          _results1 = [];
+          for (_l = 0, _len3 = tc.length; _l < _len3; _l++) {
+            tci = tc[_l];
+            _results1.push(tci.classList.add("active"));
+          }
+          return _results1;
+        })());
       }
       return _results;
     };
@@ -47,22 +53,25 @@
     return _results;
   };
 
-  trigs = document.querySelectorAll(".trigger");
-
-  trigged = document.querySelectorAll(".trigged");
-
-  triggersInit = function() {
-    var clickTrig, t, _i, _len, _results;
-    trigs = document.querySelectorAll(".trigger");
-    trigged = document.querySelectorAll(".trigged");
+  this.triggersInit = function(trigClassname, trigContentName) {
+    var clickTrig, t, trigged, trigs, _i, _len, _results;
+    if (trigClassname == null) {
+      trigClassname = "trigger";
+    }
+    if (trigContentName == null) {
+      trigContentName = "trigged";
+    }
+    trigs = document.querySelectorAll("." + trigClassname);
+    trigged = document.querySelectorAll("." + trigContentName);
     clickTrig = function(trig) {
-      var cName, td, trigName, trigState, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _results;
+      var cName, re, td, trigName, trigState, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _results;
       trigName = "";
       trigState = "off";
+      re = new RegExp(trigClassname + "\\d");
       _ref = trig.classList;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         cName = _ref[_i];
-        if (cName.match(/trigger\d/) !== null) {
+        if (cName.match(re) !== null) {
           trigName = cName;
         }
         if (cName === "off" || cName === "on") {
@@ -82,7 +91,7 @@
         trig.classList.add("on");
         trigState = "on";
       }
-      _ref1 = document.querySelectorAll(".trigged");
+      _ref1 = document.querySelectorAll("." + trigContentName);
       _results = [];
       for (_k = 0, _len2 = _ref1.length; _k < _len2; _k++) {
         td = _ref1[_k];
@@ -108,9 +117,11 @@
     return _results;
   };
 
-  window.onload = function() {
-    tabsInit();
-    return triggersInit();
-  };
+
+  /*
+  window.onload = () ->
+      tabsInit("tab")
+      triggersInit("tabContent")
+   */
 
 }).call(this);
